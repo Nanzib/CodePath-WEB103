253577da67f7c04e607abd148b4003af68c444a9
Nanzib <nchowdhury9537@bths.edu>
2025-10-09 14:16:49 -0400

    Updated

--- web103_unit3_project/project_listicle/client/src/main.js
+++
@@ -1,5 +0,0 @@
-import '@picocss/pico/css/pico.min.css';
-import './style.css';
-
-import './header.js';
-import './list.js';
--- web103_unit3_project/project_listicle/server/config/dotenv.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-import path from 'path';
-
-const envPath = path.resolve(process.cwd(), 'server', '.env');
-dotenv.config({ path: envPath });
-
-export default process.env;
